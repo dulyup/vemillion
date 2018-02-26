d9cf7848b0008919d7f0f2d467858558940cf9b7
hy yang <almond2yang@gmail.com>
2018-02-25 23:48:35 -0800

    index pathing

--- src/service/controller.js
+++ src/service/controller.js
@@ -5,7 +5,7 @@ const PORT = 2666;
 const service=require('./cardService.js');
 
 
-app.use('/',express.static(__dirname + '../public') ); 
+app.use(express.static(__dirname + '/../public') ); 
 
 app.use( bodyParser.json({ extended: true, type: '*/*' }) );
 
