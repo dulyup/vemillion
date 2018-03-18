ba842bb36d060b5606b4b3d49376cdeafc956248
dulyup <31580159+dulyup@users.noreply.github.com>
2018-03-18 15:19:04 -0700

    modify table css

--- react-client/src/Item.jsx
+++ react-client/src/Item.jsx
@@ -17,16 +17,15 @@ class Item extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            activeRow: false,
+            activeRow: true,
             word: this.props.word,
         };
-        console.log(this.state.activeRow);
     }
     render() {
         return (
             <tr className={`row ${this.state.activeRow ? 'selected' :'row'}`}
                 onClick={()=>{this.props.onClick(this.state.word["cardId"]);
-                              this.setState({activeRow: true})}}>
+                              this.setState({activeRow: !this.state.activeRow})}}>
                 <td>{this.state.word["side0"]}</td>
                 <td>{this.state.word["side1"]}</td>
             </tr>
