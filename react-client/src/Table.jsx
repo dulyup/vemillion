9c6584bb54fccf08c04326f547729c356aa12553
dulyup <31580159+dulyup@users.noreply.github.com>
2018-03-18 13:41:01 -0700

    handle empty content

--- react-client/src/Table.jsx
+++ react-client/src/Table.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 import Tbody from './Tbody';
 
 const Table = ({wordList, onClick}) => {
-    return (
-        <div>
-            <table>
-                <thead>
-                <tr>
-                    <th>Word</th>
-                    <th>Explanation</th>
-                </tr>
-                </thead>
-                <Tbody wordList={wordList}
-                       onClick={onClick}
-                />
-            </table>
-        </div>
-    )
+    if (!wordList) {
+        return (<p>No cards</p>)
+    } else {
+        return (
+            <div>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Word</th>
+                        <th>Explanation</th>
+                    </tr>
+                    </thead>
+                    <Tbody wordList={wordList}
+                           onClick={onClick}
+                    />
+                </table>
+            </div>
+        )
+    }
 };
 export default Table;
