7113017d1e218f144b4127cc4e677279ff02a6f2
Lulu Tong <lulu.tong@outlook.com>
2018-03-18 23:27:54 -0700

    modify

--- react-client/src/StudyPage.jsx
+++ react-client/src/StudyPage.jsx
@@ -3,6 +3,7 @@
 
 import React from 'react';
 import ClockTimer from './ClockTimer';
+import Banner from './Banner';
 const helper = require('./StudyPageHelperFunc.js');
 
 class StudyPage extends React.Component {
@@ -209,6 +210,12 @@ class StudyPage extends React.Component {
     const { button } = this.state;
     return (
       <div className='hidden study-page'>
+        <Banner text={'Welcome to Vermillion Flashcard! User '} id={this.props.currentUserId}/>
+        <div className="slogan">
+          <p id="slogan-study">STUDY: <span id="slogan-easier">Easier</span></p>
+          <p id="slogan-faster">Faster</p>
+          <p id="slogan-harder">Harder</p>
+        </div>
         <div id='wholecard' style={{display: (card ? 'block' : 'none')}}>
           <div id="header">Choose The Answer in <span id="time"><ClockTimer timer={this.state.timer} seconds={this.state.seconds}  callbackParent={this.onChildChanged}/></span> Seconds!</div>
           <div id="cards">
